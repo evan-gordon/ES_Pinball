fc5df3fcb8c2ad41958d275f04e1d074a98f7cf4
Evan Gordon <evanwgordon@yahoo.com>
2017-10-30 15:25:36 -0700

    draft one of displayIsr function, and fixes for old code

--- ewgordonPinball1.ino
+++ ewgordonPinball1.ino
@@ -2,27 +2,26 @@
 #define N_DIGITS   6   // each with 6 BCD digits 
 #define INCR    8353   // score incremen
 
-int DISP_ENABLED[N_DISPLAYS] = {-1};
-int DISP[N_DISPLAYS][N_DIGITS] = {-1};
+volatile int DISP_ENABLED[N_DISPLAYS] = {26, 27, 28, 29, 38};
+volatile int DISP[N_DISPLAYS][N_DIGITS] = {-1};
 
 void setup() {
   Serial.begin(9600);
+  DDRA = B11111111;
   attachInterrupt(2, dispIsr, RISING);
-  //might need to set some pins tooutput mode, see doc #6
-  
 }
 
 void loop() { // blank (not zero) all the displays by setting the score to -1 
-   
+  for (int i=0 ; i<N_DISPLAYS ; i++)
+     for (int dig=0 ; dig<N_DIGITS ; dig++)
+        DISP[i][dig] = dig ;
 }
 
 void setScore(int dispNumber, long int score)
 {
   int scoreCpy = score;
   for(int i = 0; i < N_DIGITS; ++i)
-  {
-    DISP[dispNumber][i] = removeSmallestDigit(score);
-  } 
+    {DISP[dispNumber][i] = removeSmallestDigit(score);} 
   //make leading zeros -1?
 }
 
@@ -35,11 +34,19 @@ int removeSmallestDigit(long int& score)//takes score splits off leading bit fro
 
 void dispIsr()
 {
+  static int digit = 0;
+  digitalWrite(A15, HIGH);
+  digitalWrite((39 + digit), LOW);//set previous digit to low
+  digit = (digit + 1) % 6;
+  digitalWrite(A9, HIGH);
   for(int i = 0; i < N_DISPLAYS; ++i)
   {
-    //stuff
-    digitalWrite(DISP_ENABLED[i], HIGH);
+    PORTA = DISP[i][digit];
+    digitalWrite(DISP_ENABLED[i], HIGH);//what value is supposed to be inside disp_enabled
     digitalWrite(DISP_ENABLED[i], LOW);
   }
+  digitalWrite(A9, LOW);
+  digitalWrite((39 + digit), HIGH);
+  digitalWrite(A15, LOW);
 }
 
